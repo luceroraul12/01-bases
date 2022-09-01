bd1ff66348007941e170fe9103c73822b846b2fb
Lucero Raul <luceroraul12@gmail.com>
2022-09-01 15:07:52 -0300

    gen modulo de contador y refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './heroe/heroe.component';
 import { ListadoComponent } from './heroe/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroeComponent,
-    ListadoComponent
+    ListadoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
